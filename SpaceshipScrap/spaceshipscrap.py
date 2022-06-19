03caa9373d0c3fec8954b1854896fba3e0d35cff
wiknwo <spinningthewww@outlook.com>
2022-06-19 20:00:57 +0100

    Added functionality to display winner of game, along with sound effects and background image. Game is now completely functioning.

--- SpaceshipScrap/spaceshipscrap.py
+++ SpaceshipScrap/spaceshipscrap.py
@@ -16,6 +16,7 @@ import pygame
 import os
 
 pygame.font.init() # Initializes pygame font library
+pygame.mixer.init() # Initializes pygame sound library
 
 # Defining window setting constants
 WIDTH, HEIGHT = 900, 500
@@ -30,7 +31,10 @@ YELLOW = (255, 255, 0)
 
 # Defining game setting constants
 BORDER = pygame.Rect(WIDTH // 2 - 5, 0, 10, HEIGHT) # Must subtract half the width of the border from the x coordinate to get border to appear centred.
+BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('assets', 'Grenade+1.mp3'))
+BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('assets', 'Gun+Silencer.mp3'))
 HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
+WINNER_FONT = pygame.font.SysFont('comicsans', 100)
 FRAMES_PER_SECOND = 60
 BULLET_VELOCITY = 7
 MAX_BULLETS = 3
@@ -48,6 +52,7 @@ RED_HIT = pygame.USEREVENT + 2
 
 # Functions
 def drawWindow(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
+    """Function to draw main game window"""
     WINDOW.blit(SPACE_BACKGROUND, (0, 0))
     pygame.draw.rect(WINDOW, BLACK, BORDER)
     red_health_text = HEALTH_FONT.render('Health: ' + str(red_health), 1, WHITE) # Use font to render text and 1 for color aliasing
@@ -105,6 +110,14 @@ def bulletsMovement(yellow_bullets, red_bullets, yellow, red):
             red_bullets.remove(bullet)
         elif bullet.x < 0:
             red_bullets.remove(bullet)
+
+def drawWinner(text):
+    """Function to display winner message"""
+    draw_text = WINNER_FONT.render(text, 1, WHITE)
+    WINDOW.blit(draw_text, (WIDTH // 2 - draw_text.get_width() // 2, HEIGHT // 2 - draw_text.get_height() // 2))
+    pygame.display.update()
+    pygame.time.delay(5000) # Pause the game for 5 seconds while we display winner
+
 # Game Loop
 def main():
     """Main function to run SpaceshipScrap"""
@@ -121,24 +134,30 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height // 2 - 2, 10, 5)# Want bullet to move to the right
                     yellow_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
                 if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(red.x, red.y + red.height // 2 - 2, 10, 5)# Want bullet to move to the right
                     red_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
             if event.type == RED_HIT:
                 red_health -= 1
+                BULLET_HIT_SOUND.play()
             if event.type == YELLOW_HIT:
                 yellow_health -= 1
+                BULLET_HIT_SOUND.play()
         winner_text = ""
         if red_health <= 0:
             winner_text = 'Yellow Wins!'
         if yellow_health <= 0:
             winner_text = 'Red Wins!'
         if winner_text != "":
-            pass # Someone won
+            drawWinner(winner_text) # Someone won
+            break
         # Keyboard bindings
         keys_pressed = pygame.key.get_pressed()
         yellowMovement(keys_pressed, yellow)        
@@ -146,7 +165,7 @@ def main():
         bulletsMovement(yellow_bullets, red_bullets, yellow, red) # Checking if bullets collide with characters
         drawWindow(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)
 
-    pygame.quit()
+    main()
 
 if __name__ == '__main__':
     main()
